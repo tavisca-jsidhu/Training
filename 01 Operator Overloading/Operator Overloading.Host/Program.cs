268684a652b0231fc643f5e0fafba7c6cf51f56e
Jaspreet Singh <jsidhu@tavisca.com>
2015-07-08 20:07:51 +0530

    Commit

---
+++ 01 Operator Overloading/Operator Overloading.Host/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OperatorOverloading.Model;
+
+namespace OperatorOverloading.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double amountVal;
+                string currencyVal;
+
+                Console.WriteLine("Enter 1st Amount : ");
+                double.TryParse(Console.ReadLine(),out amountVal);
+
+                Console.WriteLine("Enter Currency : ");
+                currencyVal = Console.ReadLine();
+
+                Money m1 = new Money(amountVal, currencyVal);
+
+                Console.WriteLine("Enter 1st Amount : ");
+                double.TryParse(Console.ReadLine(), out amountVal);
+
+                Console.WriteLine("Enter Currency : ");
+                currencyVal = Console.ReadLine();
+
+                Money m2 = new Money(amountVal, currencyVal);
+
+                Money m3 = m1 + m2;
+                Console.WriteLine("\n" + m3.Amount);
+                Console.WriteLine(m3.Currency);
+            }
+
+            catch (ArgumentException a)
+            {
+                Console.WriteLine(a);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01 Operator Overloading/Operator Overloading.Model/Money.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading.Model
+{
+    public class Money
+    {
+        private double _amount;
+        private string _currency;
+
+        public Money(double amountVal, string currencyVal)  // constructor
+        {
+            Amount = amountVal;
+            Currency = currencyVal;
+        }
+
+        public double Amount 
+        {
+            get
+            {
+                return this._amount;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(Messages.NegativeVal);  // throw exception if amount is negative
+                }
+                else if (double.IsPositiveInfinity(value))
+                {
+                    throw new ArgumentException(Messages.OutOfRange);
+                }
+                else
+                {
+                    this._amount = value;
+                }
+            }
+        }
+
+        public string Currency
+        {
+            get
+            {
+                return this._currency;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(Messages.NullVal);
+                }
+                else
+                {
+                    this._currency = value;
+                }
+            }
+        }
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            if (m1.Currency.Equals(m2.Currency, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Money m3 = new Money(0," ");
+
+                    m3._amount = m1._amount + m2._amount;
+                    m3._currency = m1._currency;
+                    if (m3.Amount >= double.MaxValue)
+                    {
+                        throw new ArgumentException(Messages.OutOfRange);  // throw exception if amount is negative
+                    }
+                    return m3;
+                }
+                else
+                {
+                    throw new System.Exception(Messages.NoMatch); //exception for currency
+                }
+        }
+    }
+}
