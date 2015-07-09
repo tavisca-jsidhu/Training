4ea415eb24eaf67795b81ec38bf7b22120b281f6
Jaspreet Singh <jsidhu@tavisca.com>
2015-07-09 11:28:36 +0530

    Updated Code

---
+++ 01 Operator Overloading/Operator Overloading.Host/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OperatorOverloading.Model;
+
+namespace OperatorOverloading.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double amountVal;
+                string currencyVal;
+                string input;
+                Money m1;
+                Money m2;
+
+                Console.WriteLine("Format : 'Amount currency(3 letters)'\n");
+
+                Console.WriteLine("Enter 1st Amount : ");
+
+                input = Console.ReadLine();
+
+                string[] words = input.Split(' ');
+                double.TryParse(words[0], out amountVal);
+                currencyVal = words[1];
+
+                m1 = new Money(amountVal, currencyVal);
+
+                Console.WriteLine("Enter 2nd Amount : ");
+
+                input = Console.ReadLine();
+
+                string[] words2 = input.Split(' ');
+                double.TryParse(words2[0], out amountVal);
+                currencyVal = words2[1];
+
+                m2 = new Money(amountVal, currencyVal);
+
+                Money m3 = m1 + m2;
+                Console.WriteLine("\n" + m3.Amount);
+                Console.WriteLine(m3.Currency);
+            }
+
+            catch (ArgumentException a)
+            {
+                Console.WriteLine(a);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01 Operator Overloading/Operator Overloading.Model/Money.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading.Model
+{
+    public class Money
+    {
+        private double _amount;
+        private string _currency;
+
+
+        public Money(double amountVal, string currencyVal)  // constructor
+        {
+            Amount = amountVal;
+            Currency = currencyVal;
+        }
+
+        public double Amount
+        {
+            get
+            {
+                return this._amount;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();  // throw exception if amount is negative
+                }
+                else if (double.IsPositiveInfinity(value))
+                {
+                    throw new ArgumentException(Messages.OutOfRange);
+                }
+                else
+                {
+                    this._amount = value;
+                }
+            }
+        }
+
+        public string Currency
+        {
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || (value.Length != 3))
+                {
+                    throw new ArgumentException(Messages.WrongFormat);
+                }
+                else
+                {
+                    this._currency = value;
+                }
+            }
+            get
+            {
+                return this._currency;
+            }
+        }
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            if (m1.Currency.Equals(m2.Currency, StringComparison.InvariantCultureIgnoreCase))
+            {
+                double amount = m1._amount + m2._amount;
+                string currency = m1._currency;
+                if (amount >= double.MaxValue)
+                {
+                    throw new ArgumentException(Messages.OutOfRange);  // throw exception if amount is negative
+                }
+                return new Money(amount, currency);
+            }
+            else
+            {
+                throw new System.Exception(Messages.NoMatch); //exception for currency
+            }
+        }
+    }
+}
