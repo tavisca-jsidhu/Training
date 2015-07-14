487ea6262b123fdf00a6db4f1b458dee343e1ae2
Jaspreet Singh <jsidhu@tavisca.com>
2015-07-14 11:38:01 +0530

    Web Server Added

---
+++ 02 Web Server/WebServer.Host/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServer.Model;
+
+namespace WebServer.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServer.Model.WebServer webserver = new Model.WebServer();
+            webserver.Start(8080,@"C:\Users\jsidhu\Downloads\startbootstrap-sb-admin-1.0.3");
+        }
+    }
+}
