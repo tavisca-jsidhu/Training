b7340b8c4d4a9c4544ec607e6f59cff0a4a0a111
Jaspreet Singh <jsidhu@tavisca.com>
2015-07-16 19:08:08 +0530

    commit for wcf testing

---
+++ 03 Testing/Tavisca.Testing.Model/Shapes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomAttribute;
+
+namespace Testing.Model
+{
+    [TestClass]
+    class Height
+    {
+        [TestMethod]
+        [TestCategory("Smoke Test")]
+        public void SphereHeight()
+        {
+        }
+        [TestMethod]
+        public void CubeHeight()
+        {
+        }
+        [Ignore]
+        public void CuboidHeight()
+        {
+        }
+    }
+
+    [TestClass]
+    class Length
+    {
+        [TestMethod]
+        [TestCategory("Smoke Test")]
+        public void SphereLength()
+        {
+        }
+        [Ignore]
+        public void CubeLength()
+        {
+        }
+        [TestMethod]
+        [TestCategory("Smoke Test")]
+        public void CuboidLength()
+        {
+        }
+    }
+
+    [TestClass]
+    class Breadth
+    {
+        [TestMethod]
+        public void SphereBreadth()
+        {
+        }
+        [TestMethod]
+        [TestCategory("Smoke Test")]
+        public void CubeBreadth()
+        {
+        }
+        [Ignore]
+        public void CuboidBreadth()
+        {
+        }
+    }
+}
