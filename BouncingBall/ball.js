c7b03e6d452d4384706f5a09310c3f912c42e483
Jaspreet Singh <jsidhu@tavisca.com>
2015-07-20 01:13:20 +0530

    bouncing ball commit

---
+++ BouncingBall/ball.js
@@ -0,0 +1,36 @@
+﻿var horizontal = 0;
+var vertical = 0;
+var xPos = 10;
+var yPos = 10;
+
+function start() {
+    setInterval(bounce, 20);
+}
+function bounce() {
+    var height = (window.innerHeight - 35);
+    var width = (window.innerWidth - 35);
+
+    horizontal += xPos;
+    vertical += yPos;
+    var element = document.getElementById('bounce');
+    element.style.background = "black";
+    element.style.left = previousLeft + "px";
+    element.style.top = previousTop + "px";
+
+    if (vertical >= height) {
+        yPos = -10;
+        return;
+    }
+    if (horizontal >= width) {
+        xPos = -10;
+        return;
+    }
+    if (vertical <= 0) {
+        yPos = 10;
+        return;
+    }
+    if (horizontal <= 0) {
+        xPos = 10;
+        return;
+    }
+}
