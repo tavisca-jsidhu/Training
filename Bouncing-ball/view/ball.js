50a3f112e9ceaa1f179db139d08bb0a4f42e5912
Jaspreet Singh <jsidhu@tavisca.com>
2015-07-20 22:24:59 +0530

    Bouncing ball with view

---
+++ Bouncing-ball/view/ball.js
@@ -0,0 +1,14 @@
+﻿window.ball = window.ball || { axisX: 0, axisY: 0, xPos: 10, yPos: 10 };
+
+ball.bounce = function () {
+    var element = document.getElementById('cir');
+    element.style.background = "blue";
+    element.style.left = ball.axisX + "px";
+    element.style.top = ball.axisY + "px";
+    ball.container();
+    ball.axisX += ball.xPos;
+    ball.axisY += ball.yPos;
+}
+window.ball.start = function () {
+    setInterval(ball.bounce, 15);
+}
---
+++ Bouncing-ball/view/container.js
@@ -0,0 +1,17 @@
+﻿ball.container = function () {
+
+    var height = (window.innerHeight - 70);
+    var width = (window.innerWidth - 70);
+    if (ball.axisY >= height) {
+        ball.yPos = -10;
+    }
+    if (ball.axisX >= width) {
+        ball.xPos = -10;
+    }
+    if (ball.axisY <= 0) {
+        ball.yPos = 10;
+    }
+    if (ball.axisX <= 0) {
+        ball.xPos = 10;
+    }
+}
