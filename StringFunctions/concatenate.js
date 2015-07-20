ed42b503a3c63fbc4dd862a3050a6c2976964bb9
Jaspreet Singh <jsidhu@tavisca.com>
2015-07-20 15:53:48 +0530

    updated code for string operation

---
+++ StringFunctions/concatenate.js
@@ -0,0 +1,11 @@
+﻿String.prototype.Concat = function () {
+    var stringOne = this;
+    for (var i = 0; i < arguments.length; i++) {
+        if (arguments[i] == "null")
+            stringOne += "null";
+        if (arguments[i] == "undefined")
+            stringOne += "undefined";
+        stringOne += arguments[i];
+    }
+    return stringOne;
+};
